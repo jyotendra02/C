09b0d4c703d8ddbd41c5a7f43a00d1adc0d60762
Jyotendra <85470769+jyotendra02@users.noreply.github.com>
2022-08-03 20:57:19 +0530

    gross_salary

--- gross_salary.c
+++ gross_salary.c
@@ -7,6 +7,5 @@ int main()
     dearness_allowance = 0.40 * salary;
     rent = .20 * salary;
     gross_salary = salary - (dearness_allowance + rent);
-    printf("Dearness Allowance: %0.2f\nRent allowance: %0.2f\nGross salary: %0.2f\n", dearness_allowance,rent,gross_salary);
-        
+    printf("Dearness Allowance: %0.2f\nRent allowance: %0.2f\nGross salary: %0.2f\n", dearness_allowance,rent,gross_salary);        
 }
