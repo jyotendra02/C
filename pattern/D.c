3102aed21bb0061ab120df638788644f08a1a65f
Jyotendra <85470769+jyotendra02@users.noreply.github.com>
2022-08-06 10:38:02 +0530

    pattern

--- pattern/D.c
+++ pattern/D.c
@@ -2,11 +2,26 @@
 
 int main()
 {
-    int i = 1,j = 10;
+    /*int i = 1,j = 10;
     while(i<=5)
     {
        printf(" %i %i %i %i %i \n",j,j,j,j,j);
        j+=10;
        i++;
+    }*/
+
+    int i =10;
+    while(i<=50)
+    {
+        int j=1;
+        while(j<=5)
+        {
+            printf(" %i",i);
+            j++;
+        }
+        printf("\n");
+        i+=10;
     }
+
+    
 }
